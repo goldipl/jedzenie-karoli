286eadfa69be3329dabc765bede5bb0b79464c33
goldipl <mgodlewski92@gmail.com>
2023-01-23 19:18:58 +0100

    add swiper js

---
+++ js/swiper.js
@@ -0,0 +1,19 @@
+/* MIT License */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
