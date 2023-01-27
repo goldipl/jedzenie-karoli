40c060eb4a04cad8a1a4962f7bb807b4e2d4ca24
goldipl <mgodlewski92@gmail.com>
2023-01-27 15:23:31 +0100

    handle hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// Empty file
+const burger = document.querySelector('#header .logo_burger .burger');
+const nav = document.querySelector('#header #navigation');
+const nav_el = document.querySelectorAll('#header #navigation ul li a');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('show');
+});
+
+nav_el.forEach(e => {
+    e.addEventListener('click', () => {
+        nav.classList.remove('show');
+    });
+});
